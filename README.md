046c6294e148e4114a102e0f9865433f73a7ca2d
Robert Flanagan <robertflanagan95@gmail.com>
2019-01-19 01:00:51 -0500

    setting up jawsdb

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# burger
+# Eat da Burger
+
+## How to Install Locally
+First, you will need to run `npm install` in the Terminal where `server.js` is located. If you have a MySQL shell installed, run `source schema.sql` and `source seeds.sql` to initialize and populate the database. Run the command `node server` and navigate to `http://localhost:8080/burgers` in your browser.
+
+## How to Use
+Add new burgers to the menu by typing in the name of the burger you want and clicking "Add Burger". Eat a burger by clicking "Eat da Burger!" next to the desired burger, at which point it will move over to the "Eaten Burgers" column.
--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+const keys = require("../keys.js");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: keys.mysqlCred.pw,
+        database: "burgers_db"
+    });
+}
 
 // Make connection.
 connection.connect(function(err) {
