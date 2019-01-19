c75eff0655d424908f2aac2bf2d2e2d6a1da21b6
Robert Flanagan <robertflanagan95@gmail.com>
2019-01-19 12:31:49 -0500

    changed default landing page

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -4,6 +4,9 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+  res.redirect("/burgers");
+});
 
 router.get("/burgers", function(req, res) {
   burger.selectAll(function(result) {
